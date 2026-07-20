c7c57b4b4199c862ddbbedc23e23272fe5205c0d
rhnbztnl <rhnbztnl@gmail.com>
2026-07-20 11:04:55 +0700

    revert: undo semua perubahan dari 55ece3f hingga 4f35429

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,7 +7,6 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -29,10 +28,5 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function peserta(): HasOne
-    {
-        return $this->hasOne(Peserta::class);
     }
 }
